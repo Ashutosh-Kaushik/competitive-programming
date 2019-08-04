012e11106215c8ecc81dae36503d6e3c8e24bd1a
Ashutosh-Kaushik <33092543+Ashutosh-Kaushik@users.noreply.github.com>
2019-08-04 11:50:45 +0530

    Update ncr.cpp

--- ncr.cpp
+++ ncr.cpp
@@ -4,8 +4,8 @@ const int N = 1001;
 int ncr[N][N];
 
 void f() {
-    fr(i,0,N-1) {
-        fr(j,0,N-1) {
+    fr(i,0,N) {
+        fr(j,0,N) {
             if(j == 0) {
                 ncr[i][j] = 1;
                 continue;

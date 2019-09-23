16df76a8126eff308612801a665dd4cf01e1ff6a
Ashutosh-Kaushik <33092543+Ashutosh-Kaushik@users.noreply.github.com>
2019-09-24 03:24:51 +0530

    Update cppTemplate.cpp

--- cppTemplate.cpp
+++ cppTemplate.cpp
@@ -5,55 +5,101 @@
 #include <ext/pb_ds/tree_policy.hpp> // Including tree_order_statistics_node_update
 using namespace std;
 using namespace __gnu_pbds;
+
+template <typename A, typename B>
+string to_string(pair<A, B> p);
+ 
+string to_string(const string& s) {
+  return '"' + s + '"';
+}
+ 
+string to_string(const char* s) {
+  return to_string((string) s);
+}
+ 
+string to_string(bool b) {
+  return (b ? "true" : "false");
+}
+ 
+string to_string(vector<bool> v) {
+  bool first = true;
+  string res = "{";
+  for (int i = 0; i < static_cast<int>(v.size()); i++) {
+    if (!first) {
+      res += ", ";
+    }
+    first = false;
+    res += to_string(v[i]);
+  }
+  res += "}";
+  return res;
+}
+ 
+template <size_t N>
+string to_string(bitset<N> v) {
+  string res = "";
+  for (size_t i = 0; i < N; i++) {
+    res += static_cast<char>('0' + v[i]);
+  }
+  return res;
+}
+ 
+template <typename A>
+string to_string(A v) {
+  bool first = true;
+  string res = "{";
+  for (const auto &x : v) {
+    if (!first) {
+      res += ", ";
+    }
+    first = false;
+    res += to_string(x);
+  }
+  res += "}";
+  return res;
+}
+ 
+template <typename A, typename B>
+string to_string(pair<A, B> p) {
+  return "(" + to_string(p.first) + ", " + to_string(p.second) + ")";
+}
+ 
+void debug_out() { cerr << endl; }
+ 
+template <typename Head, typename... Tail>
+void debug_out(Head H, Tail... T) {
+  cerr << " " << to_string(H);
+  debug_out(T...);
+}
+ 
+#ifdef IGM
+#define debug(...) cerr << "[" << #__VA_ARGS__ << "]:", debug_out(__VA_ARGS__)
+#else
+#define debug(...) 
+#endif
+
 #define PI                      3.1415926535897932384626
-//#define int                   long long int
-#define ll                      long long int
+#define int                     long long int
 #define fi                      first
 #define se                      second
-#define pq                      priority_queue
 #define pb                      push_back
-#define pf                      pop_front
 #define endl                    '\n'
 #define all(x)                  x.begin(),x.end()
 #define pii                     pair<int,int>
-#define fill(x,y)               memset(x,y,sizeof(x));
 #define fast                    ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
-#define hash                    _H
-#define lg                      long double         
-#define vi                      std::vector<int>
-#define vc                      std::vector<char> v;
-#define mii                     map<int,int> 
 #define SZ(x)                   ((int)x.size())
-#define fr(i,a,b)               for(int i = a; i < b; ++i)
-#define REP(i,a,b)               for(int i = a; i <= b; ++i)
-#define trav(it,v)              for(auto it = v.begin(); it != v.end(); ++it)
 #define ordered_set             tree <int, null_type,less<int>, rb_tree_tag,tree_order_statistics_node_update>
 // find_by_order(x) ==> xth element in the set counting from 0
 // order_of_key(x) ==> number of element strictly less than x
 
-template<typename T> void println(T &x)                             {cout << x << endl;}
-template<typename T> void prints(T &x)                              {cout << x << " ";}
-template<typename T> void print(T &x)                               {cout << x;}    
-
-template<typename T> void pv(std::vector<T> &v)                     {for(auto i:v) cout << i << " "; cout << endl;}
-template<typename T> void pv1(std::vector<T> &v)                    {for(int i = 1; i < SZ(v); ++i) cout << v[i] << " "; cout << endl;}
-template<typename T> void pvln(std::vector<T> &v)                   {for(auto i:v) cout << i << endl;}
-template<typename T> void pvln1(std::vector<T> &v)                  {for(int i = 1; i < SZ(v); ++i) cout << v[i] << endl;}
-
-template<typename T> void p_set(set<T> s)                           {for(auto i:s) cout << i << " "; cout << endl;}
-
-template<typename T> void debug(std::vector<T> &v)                  {for(auto i:v) cerr << i <<" "; cerr << endl;}
-template<typename T> void d2(T &v)                                  {cerr << v << endl;}
-
-
-template<typename T> void in(std::vector<T> &v,int n)               {for(int i = 0; i < n; ++i) cin >> v[i];}
-template<typename T> void in1(std::vector<T> &v,int n)              {for(int i = 1; i <= n; ++i) cin >> v[i];}
+typedef long long ll;
+typedef long double ld;
 
 template<typename T> T pow(T a,T b, int m){T ans=1; while(b>0){ if(b%2==1) ans=(ans*a)%m; b/=2; a=(a*a)%m; } return ans%m; }
-template<typename T> T InverseMod(T a,int m){return pow(a,m-2,m);}
+template<typename T> T InverseMod(T a,T m){return pow(a,m-2,m);}
 template<typename T>int SquareDist(T a,T b){int x = abs(a.fi - b.fi),y = abs(a.se - b.se); return x*x + y*y;}
-
-
+template<typename T> T pow2(T a,T b){T ans=1; while(b>0){ if(b%2==1) ans=(ans*a); b/=2; a=(a*a); } return ans; }
+ 
 struct custom_hash {
     static uint64_t splitmix64(uint64_t x) {
         // http://xorshift.di.unimi.it/splitmix64.c
@@ -62,30 +108,29 @@ struct custom_hash {
         x = (x ^ (x >> 27)) * 0x94d049bb133111eb;
         return x ^ (x >> 31);
     }
-
+ 
     size_t operator()(uint64_t x) const {
         static const uint64_t FIXED_RANDOM = chrono::steady_clock::now().time_since_epoch().count();
         return splitmix64(x + FIXED_RANDOM);
     }
 };
-
-unordered_map<long long, int, custom_hash> safe_map;
-
+ 
+ 
+// Write only proved sloutions
 
 
 
 void solve() {
-    
-}  
- 
- 
-signed main()
+        
+}
+
+
+int32_t main()
 {
-    //freopen("input.txt", "r", stdin);
-    //freopen("output.txt", "w", stdout);
-    fast;
-    int t = 1;
-    //cin >> t;
-    while(t--)
-        solve();
+        fast;
+        int t = 1;
+        // cin >> t;
+        for(int i = 1; i <= t; ++i)
+                solve();
+        cerr << "Time taken: " << ((clock() * 1000) / CLOCKS_PER_SEC) << "ms\n";
 }
